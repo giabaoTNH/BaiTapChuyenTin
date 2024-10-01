b4246d7450ad3d60487dc0b0abebb8b4e9300f6d
giabaoTNH <hanguyengiabao1234@gmail.com>
2024-10-01 13:14:07 +0700

    add 2.10.py and 2.13.py

--- Quyen1/2.10.py
+++ Quyen1/2.10.py
@@ -1,9 +1,20 @@
 a,b = map(int,input().split())
 fib = [1]*2
-sum = 0
+sum = (a==0)
 while fib[-1] <= b:
     if fib[-1] >= a and fib[-1] <= b:
         sum+=1
     fib.append(fib[-1]+fib[-2])
 print(sum)
+# cach 2
+f = [0]+[1]
+a,b = map(int,input().split())
+ans = (a==0)
+while f[-1] <= b:
+    f.append(f[-1]+f[-2])
+    if (f[-1] >= a and f[-1] <=b):
+        ans+=1
+
+print(ans)
+
 
