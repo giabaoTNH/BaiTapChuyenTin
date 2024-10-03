c859b9caaffc0459504e90346e46da7258547668
giabaoTNH <hanguyengiabao1234@gmail.com>
2024-10-03 21:45:00 +0700

    add

--- Quyen1/2.1.py
+++ Quyen1/2.1.py
@@ -1,15 +1,17 @@
 a = input()
 b = []
 mp = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E','F']
-while(len(a) % 4 != 0):
-    a = '0'+a
+
+a = bool(len(a) % 4)*(4 - (len(a)%4))*'0' + a
 i = len(a) - 1
+
 while i>=0:
     d = 0
     for j in range(4):
-        d += pow(2,j)*int(a[i])
+        d += int(a[i]) << j
         i -= 1
     b.append(d)
-for x in reversed(b):
+
+for x in b[::-1]:
     print(mp[x],end='')
 
