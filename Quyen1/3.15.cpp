7d5edd68c0239ec38cfc9e99e8f250990055e38d
giabaoTNH <hanguyengiabao1234@gmail.com>
2024-11-29 10:33:06 +0700

    s

--- Quyen1/3.15.cpp
+++ Quyen1/3.15.cpp
@@ -11,11 +11,13 @@ bool comp (string s1, string s2)
 
 int main() 
 {
-	freopen("input.txt","r",stdin);
-	freopen("output.txt","w",stdout);
-	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	//Nhap xuat
 	cin >> n;
 	for (int i = 1; i<=n; i++) cin >> s[i];
+
 	sort(s+1,s+n+1,comp);
 	for (int i = 1; i<=n; i++) cout << s[i];
 	cout << endl;
